643d8d9a2461ed61c87bd3fa8deaed653d95e65b
vishnu vardhan reddy <vishnureddynani11@gmial.com>
2021-06-16 09:13:19 +0530

    secure commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,7 @@ import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 // import { firebaseConfig } from '../assets/utils/firebase.js';
 import { AddPhotoComponent } from './components/add-photo/add-photo.component';
-
+import { firebaseConfig } from '../../config'
 
 @NgModule({
   declarations: [
@@ -53,14 +53,7 @@ import { AddPhotoComponent } from './components/add-photo/add-photo.component';
     AppRoutingModule,
 
     
-    AngularFireModule.initializeApp({
-      apiKey: "AIzaSyBSRVHJfzUdKDnmOcXYam3lSCOUGeukXxk",
-      authDomain: "webapp-92251.firebaseapp.com",
-      projectId: "webapp-92251",
-      storageBucket: "webapp-92251.appspot.com",
-      messagingSenderId: "351004864826",
-      appId: "1:351004864826:web:b4c7419ad8c438dc246003"
-    }),
+    AngularFireModule.initializeApp(firebaseConfig),
     AngularFirestoreModule, // firestore
     AngularFireAuthModule, // auth
     AngularFireStorageModule // storage
